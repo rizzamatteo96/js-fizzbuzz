3752c9ce1fe2176d5777450724296006422c4628
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-06-03 14:33:29 +0200

    add stampa dei numeri da 0 a 100

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi anche "Fizz" di fianco al numero e per i multipli di 5 stampi "Buzz". Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz".
+
+// 1. stampiamo i numeri da 1 a 100
+for (var i = 1; i < 100; i++){
+    console.log(i);
+
+    // 2. verificare SE multipli di 3 o 5 o entrambi e aggiungere Fit o Buzz
+
+
+
+}
