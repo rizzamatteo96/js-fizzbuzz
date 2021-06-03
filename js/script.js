bb05dd308be9cc17814019b0e581137400cc1fdb
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-06-03 14:42:17 +0200

    add if to check multiple of 3 or 5

--- js/script.js
+++ js/script.js
@@ -2,10 +2,19 @@
 
 // 1. stampiamo i numeri da 1 a 100
 for (var i = 1; i < 100; i++){
-    console.log(i);
+    // console.log(i);
 
     // 2. verificare SE multipli di 3 o 5 o entrambi e aggiungere Fit o Buzz
+    if (i % 3 == 0 && i % 5 == 0){
+        var msg = i + ' - FizzBuzz';
+    } else if (i % 3 == 0){
+        var msg = i + ' - Fizz';
+    } else if (i % 5 == 0){
+        var msg = i + ' - Buzz';
+    } else {
+        var msg = i;
+    }
 
-
+    document.getElementById('risultato').innerHTML += msg + '<br>';
 
 }
